956d5107f710cbbfe158e0cd07b4bb51d2a4ec40
christian-reynolds <93774940+christian-reynolds@users.noreply.github.com>
2022-01-21 13:32:31 -0500

    Added the getBalances function

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var cors = require('cors');
 const { getMerkleRoot, getProof } = require('./merkletree');
+const { getBalances } = require("./web3");
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.use(cors())
 
@@ -17,10 +20,16 @@ app.get('/merkle', function (req, res) {
 // This would get called from the React application with a single address when someone is trying to claim their NFT
 // Example URL:
 // http://localhost:8081/merkle/0x41fACac9f2aD6483a2B19F7Cb34Ef867CD17667D
-app.get('/merkle/:address', function (req, res) {
+app.get('/merkle/:address', async function (req, res) {
     let address = req.params.address;
+
     console.log(address);
     res.end(getProof(address));
+
+    // This is an example of calling getBalances.  DO NOT DELETE
+    // let test = await getBalances(address);
+    // console.log(test);
+    // res.send({ test });
 })
 
 var server = app.listen(8081, function () {
